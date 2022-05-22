79b7e8894aba58f1443b6de70ec1e5d58a3c7bc4
Shaila Nasrin <drshaila2021@gmail.com>
2022-05-22 13:34:38 -0500

    login with email and password

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useForm } from "react-hook-form";
-import { Link, Navigate, useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import {
   useSignInWithEmailAndPassword,
   useSignInWithGoogle,
